4e7354caf3952d3602e1cd3f25040e181858f17f
markiplagat <markkiplagatkipruttoh@gmail.com>
2026-03-04 08:00:20 +0000

    auth urls and settings

--- pressure_monitor/config/settings.py
+++ pressure_monitor/config/settings.py
@@ -123,3 +123,8 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Authentication settings
+LOGIN_URL = "/accounts/login/"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
