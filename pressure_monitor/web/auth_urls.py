d745413c7df5bc0015a0d273b04b36704e344be1
markiplagat <markkiplagatkipruttoh@gmail.com>
2026-03-05 11:01:22 +0000

    fix demo login url

--- pressure_monitor/web/auth_urls.py
+++ pressure_monitor/web/auth_urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("login/", auth_views.LoginView.as_view(template_name="auth/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("signup/", signup, name="signup"),
+    path("demo-login/<str:role>/", demo_login, name="demo_login"),
 ]
