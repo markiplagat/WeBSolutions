4e0bac5ea1811c3da482ea59db4bc5e24cd5b37c
markiplagat <markkiplagatkipruttoh@gmail.com>
2026-04-22 11:55:10 +0100

    Revert "Merge lovel branch: admin panel, patient readings, flagged events"
    This reverts commit 12ad7992941219b52ea4b4f085d53ad2c73d3be2, reversing
    changes made to 659cf5f87432d782f4f0d6527f56b5eb487dae3d.

--- pressure_monitor/web/urls.py
+++ pressure_monitor/web/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-from .views import home, patient_dashboard, clinician_flagged_events
+from .views import home
 
 urlpatterns = [
     path("", home, name="home"),
-    path("patient/dashboard/", patient_dashboard, name="patient_dashboard"),
-    path("clinician/flagged-events/", clinician_flagged_events, name="clinician_flagged_events"),
 ]
