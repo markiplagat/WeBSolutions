4cf9633fe98b9cabf14e86e66cee5eda62a09f47
Lovelbittok <markiplagat@users.noreply.github.com>
2026-04-23 09:05:01 +0300

    Update admin panel view

--- pressure_monitor/web/views_adminpanel.py
+++ pressure_monitor/web/views_adminpanel.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-
+# The login page for Admin
 @login_required
 def admin_dashboard(request):
     context = {
